acf686a367078694fe64ca5569950b02e7cf83dc
normanyung <normanyung@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-23 21:22:32 +0000

    changing select option groups html to use refactored function.
    git-svn-id: https://plugins.svn.wordpress.org/sexybookmarks/trunk@148472 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html-helpers.php
+++ html-helpers.php
@@ -9,4 +9,36 @@ function sexy_network_input_select($name, $hint) {
 		$name
 	);
 }
+
+// returns the option tag for a form select element
+// $opts array expecting keys: field, value, text
+function sexy_form_select_option($opts) {
+	global $sexy_plugopts;
+	$opts=array_merge(
+		array(
+			'field'=>'',
+			'value'=>'',
+			'text'=>'',
+		),
+		$opts
+	);
+	return sprintf('<option%s value="%s">%s</option>'
+		($sexy_plugopts[$opts['field']]==$opts['value'])?' selected="selected"':"",
+		$opts['value'],
+		$opts['text']
+	);
+}
+
+// given an array $options of data and $field to feed into sexy_form_select_option
+function sexy_select_option_group($field, $options) {
+	$h='';
+	foreach ($options as $value=>$text) {
+		$h.=sexy_form_select_option(array(
+			'field'=>$field,
+			'value'=>$value,
+			'text'=>$text,
+		));
+	}
+	return $h;
+}
 ?>
