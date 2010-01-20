ca21994cf13d97915a42d03329d50493ef31df2e
Josh Jones <Josh Jones@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-20 10:57:54 +0000

    Removed un-needed section in public.php
    git-svn-id: https://plugins.svn.wordpress.org/sexybookmarks/trunk@195716 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/public.php
+++ includes/public.php
@@ -334,20 +334,6 @@ function get_sexy() {
 					'permalink'=>$perms,
 				));
 				break;
-			case 'sexy-dzone':
-				$socials.=bookmark_list_item($name, array(
-					'title'=>$mail_subject,
-					'post_summary'=>$post_summary,
-					'permalink'=>$perms,
-				));
-				break;
-			case 'sexy-boxnet':
-				$socials.=bookmark_list_item($name, array(
-					'title'=>$mail_subject,
-					'post_summary'=>$post_summary,
-					'permalink'=>$perms,
-				));
-				break;
 			case 'sexy-diigo':
 				$socials.=bookmark_list_item($name, array(
 					'sexy_teaser'=>$sexy_content,
