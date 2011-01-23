160877241c09c66aa708074a5a912dc1ab727ef8
shareaholic <shareaholic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-23 22:37:49 +0000

    - v3.3.2
    git-svn-id: https://plugins.svn.wordpress.org/sexybookmarks/trunk@336209 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: shareaholic
 Tags: sexybookmarks,sexy bookmarks,sexy,social bookmarking,social,bookmarks menu,bookmarking,share,goo.gl,googl,url shortener,bit.ly,bitly,sharethis,sharing,saving,posting,sharing is sexy,google,google buzz,buzz it,button,seo,stats, digg,delicious,diigo,myspace,twitter,facebook,gmail,email,e-mail,technorati,reddit,stumbleupon,yahoo,shareaholic,addtoany,sharedaddy,sociable,wpmu
 Requires at least: 2.7
 Tested up to: 3.0.4
-Stable tag: 3.3.1
+Stable tag: 3.3.2
 Adds an attractive social bookmarking menu to your posts, pages, index, or any combination of the three.
 
 
@@ -108,6 +108,9 @@ Please see here: [Usage & Installation Instructions](http://sexybookmarks.sharea
 
 
 == Changelog ==
+
+= 3.3.2 =
+* Quick fix for admin menu PHP error
 
 = 3.3.1 =
 * Fix for W3C validation errors (thanks to [Maitre Mo](http://maitremo.fr))
--- sexy-bookmarks.php
+++ sexy-bookmarks.php
@@ -3,7 +3,7 @@
 Plugin Name: SexyBookmarks (by Shareaholic)
 Plugin URI: http://www.shareaholic.com/tools/wordpress/
 Description: SexyBookmarks adds a (X)HTML compliant list of social bookmarking icons to each of your posts. See <a href="admin.php?page=sexy-bookmarks.php">configuration panel</a> for more settings.
-Version: 3.3.1
+Version: 3.3.2
 Author: Shareaholic
 Author URI: http://www.shareaholic.com
 
@@ -12,7 +12,7 @@ Author URI: http://www.shareaholic.com
 */
 
 
-define('SHRSB_vNum','3.3.1');
+define('SHRSB_vNum','3.3.2');
 
 // Check for location modifications in wp-config
 // Then define accordingly
@@ -841,7 +841,7 @@ function shrsb_menu_link() {
 		    'administrator', basename(__FILE__), 'shrsb_settings_page', SHRSB_PLUGPATH.'images/shareaholic_16x16.png');
 		            
 		add_submenu_page( basename(__FILE__), __( 'SexyBookmarks (by Shareaholic)' ), __( 'SexyBookmarks', 'shrsb' ),
-    		'administrator', basename(__FILE__), 'shrsb_settings_page_main' );
+    		'administrator', basename(__FILE__), 'shrsb_settings_page' );
     	
 		add_action( "admin_print_scripts-$shrsb_admin_page", 'shrsb_admin_scripts' );
 		add_action( "admin_print_styles-$shrsb_admin_page", 'shrsb_admin_styles' );
