ca6fdb2e2565e29ee6e9f89ad054f89e6fa95ed9
shareaholic <shareaholic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-21 03:57:11 +0000

    Tagging v6.1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/sexybookmarks/tags/6.1.2.0@656079 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/shrsb_sexybookmarks_page.php
+++ includes/shrsb_sexybookmarks_page.php
@@ -15,58 +15,55 @@ function shrsb_sb_set_options($action = NULL){
     *   @desc Most Popular Services List
     *   @note To change the most popular list also change the "Most Popular" link click handler in shareaholic-admin.js
     */
+    
     $shrsb_most_popular = array (
-        'shr-facebook',
-        'shr-twitter',
-        'shr-linkedin',
-		'shr-googleplus',
-        'shr-googlebookmarks',
-        'shr-stumbleupon',
-		'shr-fastmail',
-        'shr-printfriendly'
+			'shr-facebook',
+			'shr-twitter',
+			'shr-linkedin',
+			'shr-googleplus',
+			'shr-googlebookmarks',
+			'shr-stumbleupon',
+			'shr-fastmail',
+			'shr-printfriendly'
     );
     
     $defaultLikeButtonOrder = array(
-        'shr-fb-like',
-        'shr-fb-send',
-        'shr-plus-one',
-        'shr-tw-button'
+			'shr-fb-like',
+			'shr-fb-send',
+			'shr-plus-one',
+			'shr-tw-button'
     );
     			               
     $shrsb_sb_plugopts_default = array(
 			'sexybookmark' => '0',
-        
-            'firstrun' => '1',
-        
+			'firstrun' => '1',  
 			'position' => 'below', // below, above, or manual
 			'reloption' => 'nofollow', // 'nofollow', or ''
 			'targetopt' => '_blank', // 'blank' or 'self'
 			'perfoption' => '1', // Third party Content
 			'showShareCount' => '1', // fb/twit share count
-
-            'likeButtonSetTop' => '0', // Include like button below the Post Title
-            'fbLikeButtonTop' => '0', // Include fb like button
-            'fbSendButtonTop' => '0', // Include fb like button
-            'googlePlusOneButtonTop' => '0', // Include Google Plus One button
-            'tweetButtonTop' => '0', // Include Tweet button
-            'likeButtonSetSizeTop' => "1", // Size of like buttons
-            'likeButtonSetCountTop' => "true", // Show count with +1 button
-            'likeButtonOrderTop' => $defaultLikeButtonOrder,
-            'likeButtonSetAlignmentTop' => '0', // Alignment 0 => left, 1 => right
-
-            'likeButtonSetBottom' => '1', // Include like button below the Post
-            'fbLikeButtonBottom' => '0', // Include fb like button
-            'fbSendButtonBottom' => '0', // Include fb like button
-            'googlePlusOneButtonBottom' => '0', // Include Google Plus One button
-            'tweetButtonBottom' => '0', // Include Tweet button
-            'likeButtonSetSizeBottom' => "1", // Size of like buttons
-            'likeButtonSetCountBottom' => "true", // Show count with +1 button
-            'likeButtonOrderBottom' => $defaultLikeButtonOrder,
-            'likeButtonSetAlignmentBottom' => '0', // Alignment 0 => left, 1 => right
+			'likeButtonSetTop' => '0', // Include like button below the Post Title
+			'fbLikeButtonTop' => '0', // Include fb like button
+			'fbSendButtonTop' => '0', // Include fb like button
+			'googlePlusOneButtonTop' => '0', // Include Google Plus One button
+			'tweetButtonTop' => '0', // Include Tweet button
+			'likeButtonSetSizeTop' => "1", // Size of like buttons
+			'likeButtonSetCountTop' => "true", // Show count with +1 button
+			'likeButtonOrderTop' => $defaultLikeButtonOrder,
+			'likeButtonSetAlignmentTop' => '0', // Alignment 0 => left, 1 => right
+			'likeButtonSetBottom' => '1', // Include like button below the Post
+			'fbLikeButtonBottom' => '0', // Include fb like button
+			'fbSendButtonBottom' => '0', // Include fb like button
+			'googlePlusOneButtonBottom' => '0', // Include Google Plus One button
+			'tweetButtonBottom' => '0', // Include Tweet button
+			'likeButtonSetSizeBottom' => "1", // Size of like buttons
+			'likeButtonSetCountBottom' => "true", // Show count with +1 button
+			'likeButtonOrderBottom' => $defaultLikeButtonOrder,
+			'likeButtonSetAlignmentBottom' => '0', // Alignment 0 => left, 1 => right
 			'locale'=> '0', //Default locale set to 0
-            'fbNameSpace' => '1',  // Add fb name space to the html
-            'preventminify' => '1',  // prevent wp_minify from minifying the js
-            'shrlink' => '0', // show promo link
+			'fbNameSpace' => '1',  // Add fb name space to the html
+			'preventminify' => '1',  // prevent wp_minify from minifying the js
+			'shrlink' => '0', // show promo link
 			'bgimg-yes' => 'yes', // 'yes' or blank
 			'mobile-hide' => '', // 'yes' or blank
 			'bgimg' => 'caring', // default bg image
@@ -77,21 +74,21 @@ function shrsb_sb_set_options($action = NULL){
 			'expand' => '1',
 			'autocenter' => '1',
 			'tweetconfig' => urlencode('${title} - ${short_link} via @Shareaholic'), // Custom configuration of tweet
-		    'warn-choice' => '',
+		  'warn-choice' => '',
 			'doNotIncludeJQuery' => '',
 			'custom-mods' => '',
 			'scriptInFooter' => '',
-            'shareaholic-javascript' => '1',
-            'shrbase' => 'http://www.shareaholic.com',
-            'apikey' => '8afa39428933be41f8afdb8ea21a495c',
-            'service' => '',
-            'designer_toolTips' => '1',
-            'tip_bg_color' => '#000000',  // tooltip background color
-            'tip_text_color' => '#ffffff', // tooltip text color
-            'spritegen_path' => SHRSB_UPLOADDIR_DEFAULT,
-            'ogtags' => '1',  //OgTags
-            'promo' => '1'
-        );
+      'shareaholic-javascript' => '1',
+      'shrbase' => 'http://www.shareaholic.com',
+      'apikey' => '8afa39428933be41f8afdb8ea21a495c',
+      'service' => '',
+      'designer_toolTips' => '1',
+      'tip_bg_color' => '#000000',  // tooltip background color
+      'tip_text_color' => '#ffffff', // tooltip text color
+      'spritegen_path' => SHRSB_UPLOADDIR_DEFAULT,
+      'ogtags' => '1',  //Open Graph Tags
+      'promo' => '1'
+		);
     
         //Return default settings 
         if($action == "reset"){
@@ -115,6 +112,8 @@ function shrsb_sb_set_options($action = NULL){
                     $database_Settings['firstrun'] = '0';
                     $need_to_update = true;
                 }
+                //For first time activation
+                update_option("SHR_activate",1);
 
                 //Check whether all the settings are present or not
                 foreach($shrsb_sb_plugopts_default as $k => $v){
@@ -150,7 +149,6 @@ add_option('SHRSB_DefaultSprite',true);
 
 // If plugin is upgrading
 if(SHRSB_UPGRADING == TRUE) {
-    
 
     //Remove the Disabled Services
     if(isset ($shrsb_plugopts) && isset($shrsb_plugopts['service'])){
@@ -158,14 +156,48 @@ if(SHRSB_UPGRADING == TRUE) {
 
        if(!empty($services)){
            // Removing blocked services from sb services list
-           $disable_services = array( '4', '12', '68', '77', '159', '185', '186', '195', '207', '237', '257' );
+           $disable_services = array( '4', '12', '68', '77', '159', '185', '186', '195', '207', '237', '257', '264', '190', '10', '287', '188', '100', '277' );
            $services = array_diff($services, $disable_services);
            $shrsb_plugopts['service'] = implode(',', $services );
        }
-    }    
+    }
+    
+    if(isset ($shrsb_plugopts) && isset($shrsb_plugopts['bookmark'])){
+      // Removing blocked services from bookmarks list
+      
+      $shrsb_plugopts['bookmark'] = array_diff($shrsb_plugopts['bookmark'], array( 'shr-twittley', 'shr-comfeed', 'shr-ning', 'shr-strands', 'shr-sphinn', 'shr-tipd', 'shr-faqpal', 'shr-technorati', 'shr-yahoobuzz' ));
+    }
+    
     if(isset ($shrsb_plugopts) && isset($shrsb_plugopts['reloption']) && $shrsb_plugopts['reloption'] === "" ){
         $shrsb_plugopts['reloption'] = '1';
     }
+    
+    // Some databases got corrupted. This will set things in place.
+    if($shrsb_plugopts['shrbase'] != 'http://www.shareaholic.com'){
+        $shrsb_plugopts['shrbase'] = 'http://www.shareaholic.com';
+    }
+
+    // Reset depreciated url shorteners
+    if($shrsb_plugopts['shorty'] == 'slly' || $shrsb_plugopts['shorty'] == 'cligs' || $shrsb_plugopts['shorty'] == 'snip' || $shrsb_plugopts['shorty'] == 'tinyarrow' || $shrsb_plugopts['shorty'] == 'b2l' || $shrsb_plugopts['shorty'] == 'trim' || $shrsb_plugopts['shorty'] == 'e7t' || $shrsb_plugopts['shorty'] == 'googl')  {
+        $shrsb_plugopts['shorty'] = 'google';
+
+        //reset depreciated settings
+        $shrsb_plugopts['shortyapi']['snip']['user'] = '';
+        $shrsb_plugopts['shortyapi']['snip']['key'] = '';
+        $shrsb_plugopts['shortyapi']['trim']['chk'] = '';
+        $shrsb_plugopts['shortyapi']['trim']['user'] = '';
+        $shrsb_plugopts['shortyapi']['trim']['pass'] = '';
+        $shrsb_plugopts['shortyapi']['tinyarrow']['chk'] = '';
+        $shrsb_plugopts['shortyapi']['tinyarrow']['user'] = '';
+        $shrsb_plugopts['shortyapi']['cligs']['chk'] = '';
+        $shrsb_plugopts['shortyapi']['cligs']['key'] = '';
+    }
+
+    if($shrsb_plugopts['shorty'] == 'tiny') {
+        $shrsb_plugopts['shorty'] = 'tinyurl';
+    }
+    
+    update_option('SexyBookmarks', $shrsb_plugopts);
 }
 
 /*
@@ -199,10 +231,5 @@ if(isset($_POST['save_changes_sb']) ){
 
 $shrsb_plugopts['apikey'] = get_option('SHRSB_apikey');
 $shrsb_custom_sprite = get_option('SHRSB_CustomSprite');
-
-// Some databases got corrupted. This will set things in place.
-if($shrsb_plugopts['shrbase'] != 'http://www.shareaholic.com'){
-    $shrsb_plugopts['shrbase'] = 'http://www.shareaholic.com';
-}
 
 ?>
